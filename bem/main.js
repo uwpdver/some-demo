fe4067fb23c002397a56bf1ab85ef56a3eb00bf3
lzh <1119548217@qq.com>
2020-09-23 10:56:45 +0800

    feat：优化响应性；

--- bem/main.js
+++ bem/main.js
@@ -1,5 +1,5 @@
 var card = document.querySelector('.c-card');
 card.addEventListener( 'click', function() {
   card.classList.toggle('is-flipped');
-  document.querySelector('.c-card__desc--back').classList.toggle('c-card__desc--flipped');
+  document.querySelector('.c-card__face--back').classList.toggle('c-card__face--flipped');
 });
